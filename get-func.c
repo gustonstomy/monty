0d73264d1c762c5c7de823f66d2b48f0e3533c44
Stomy1 <110528133+gustonstomy@users.noreply.github.com>
2023-12-15 08:19:01 +0000

    Update get-func.c

--- get-func.c
+++ get-func.c
@@ -12,7 +12,7 @@ int _get_function_stack(char **tokens, stack_t **head, unsigned int increament)
 	instruction_t ops[] = { {"push", v_push}, {"pall", v_pall}, {"pint", v_pint},
 		{"pop", v_pop}, {"swap", v_swap}, {"add", v_add}, {"sub", v_sub},
 		{"div", v_div}, {"mul", v_mul}, {"mod", v_mod}, {"pchar", v_pchar},
-		{"pstr", v_pstr}, {"rotl", v_rotl}, {"rotr", v_rotr}, {"stack", v_stack},
+		{"pstr", v_pstr}, {"rotl", v_rotl}, {"rotr", v_rotr},
 		{"queue", v_queue}, {"nop", v_nop}, {"\n", v_nop}, {NULL, NULL} };
 	int i = 0, j = 0, find_func = 0, number = 0, out = 0;
 
