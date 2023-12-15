5936b27aaf120ec3a7c14121f46511dc13f74e37
Stomy1 <110528133+gustonstomy@users.noreply.github.com>
2023-12-15 09:32:51 +0000

    Update push.c

--- push.c
+++ push.c
@@ -1,15 +1,15 @@
 #include "monty.h"
 /**
- * v_push - to push into the stack
+ * _push - to push into the stack
  * @head: pointer to pointer to stack
  * @increament: value to include
  * Return: Nothing
  */
-void v_push(stack_t **head, unsigned int increament)
+void _push(stack_t **head, unsigned int increament)
 {
 	if (if_stack)
-		v_add_stack(head, increament);
+		add_stack(head, increament);
 	else
-		v_add_queue(head, increament);
+		add_queue(head, increament);
 }
 
