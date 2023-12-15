fd044f8567666751512302d580af1ecd2eabea35
Stomy1 <110528133+gustonstomy@users.noreply.github.com>
2023-12-15 09:28:43 +0000

    Update pal.c

--- pal.c
+++ pal.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 /**
- * v_pall - to print all elements in a stack
+ * _pall - to print all elements in a stack
  * @head: pointer to pointer to stack
  * @increament: value to print
  * Return: Nothing
  */
-void v_pall(stack_t **head, __attribute__((unused))unsigned int increament)
+void _pall(stack_t **head, __attribute__((unused))unsigned int increament)
 {
 	stack_t *aux = *head;
 
