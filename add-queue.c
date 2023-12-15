51ab9bab6b8b1b13a61c1ec442b4e5404c3a1d84
Stomy1 <110528133+gustonstomy@users.noreply.github.com>
2023-12-15 09:08:05 +0000

    Update add-queue.c

--- add-queue.c
+++ add-queue.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 /**
- * _add_queue - adds node to stack
+ * add_queue - adds node to stack
  * @head: pointer to pointer in stack
  * @n: element
  * Return: Nothing
  */
-stack_t *_add_queue(stack_t **head, const int n)
+stack_t *add_queue(stack_t **head, const int n)
 {
 	stack_t *current, *previ;
 
