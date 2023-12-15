df54db558a19c1e6959724518a8d3a0a513217fc
Stomy1 <110528133+gustonstomy@users.noreply.github.com>
2023-12-15 09:35:47 +0000

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 /**
- * v_stack - sformat of stack.
+ * _stack - sformat of stack.
  * @head: pointer to pointer of stack
  * @increadment: element
  * Return: Nothing.
  */
-void v_stack(stack_t **head, unsigned int increadment)
+void _stack(stack_t **head, unsigned int increadment)
 {
 	(void)head, (void)increadment;
 
