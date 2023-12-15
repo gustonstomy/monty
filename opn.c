ad937a366da120cfd6733139ca5264dc65a4eafa
Stomy1 <110528133+gustonstomy@users.noreply.github.com>
2023-12-15 09:27:37 +0000

    Update opn.c

--- opn.c
+++ opn.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 /**
- * v_nop - nop doesn’t do anything.
+ * _nop - nop doesn’t do anything.
  * @head: pointer to pointer in stack
  * @increament: element
  * Return: Nothing
  */
-void v_nop(stack_t **head, unsigned int increament)
+void _nop(stack_t **head, unsigned int increament)
 {
 	(void)head;
 	(void)increament;
