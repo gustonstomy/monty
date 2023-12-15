4a59976a6a9a73dfce698ef61e0b1bc53de78925
Stomy1 <110528133+gustonstomy@users.noreply.github.com>
2023-12-15 09:11:58 +0000

    Update add.c

--- add.c
+++ add.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 /**
- * _add - adds element to stack
+ * add - adds element to stack
  * @head: pointer to pointer in stack
  * @increament: element to add
  * Return: Nothing
  */
-void _add(stack_t **head, unsigned int increament)
+void add(stack_t **head, unsigned int increament)
 {
 	int total = 0;
 
