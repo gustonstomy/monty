696b5640358979966464a5eacea9b31590ae06df
Stomy1 <110528133+gustonstomy@users.noreply.github.com>
2023-12-15 09:33:16 +0000

    Update quee.c

--- quee.c
+++ quee.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 /**
- * v_queue - formst for stack
+ * _queue - formst for stack
  * @head: pointer to pointer to stack.
  * @increament: element
  * Return: Nothing.
  */
-void v_queue(stack_t **head, unsigned int increament)
+void _queue(stack_t **head, unsigned int increament)
 {
 	(void)head, (void)increament;
 
