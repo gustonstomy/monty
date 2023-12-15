0908aed9ac8760f2fa8265a712527d35a0a4dbb5
Stomy1 <110528133+gustonstomy@users.noreply.github.com>
2023-12-15 09:56:56 +0000

    Update monty.c

--- monty.c
+++ monty.c
@@ -17,12 +17,12 @@ int main(int argc, char *argv[])
 
 	STATUS[0] = 'S', if_stack = 1;
 	if (argc != 2)
-	{	dprintf(2, "USAGE: monty file\n");
+	{	vprintf(2, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	fd = fopen(argv[1], "r");
 	if (fd == NULL)
-	{	dprintf(2, "Error: Can't open file %s\n", argv[1]);
+	{	vprintf(2, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while ((getline(&buffer, &size, fd)) != -1)
@@ -34,10 +34,10 @@ int main(int argc, char *argv[])
 			gf = get_function_stack(tokens, &stack, lines);
 			if (gf == 1)
 			{	free(tokens), free(buffer), free_stack(&stack), fclose(fd);
-				dprintf(2, "L%d: usage: push integer\n", lines), exit(EXIT_FAILURE);
+				vprintf(2, "L%d: usage: push integer\n", lines), exit(EXIT_FAILURE);
 			}
 			if (gf == 2)
-			{	dprintf(2, "L%d: unknown instruction %s\n", lines, tokens[0]);
+			{	vprintf(2, "L%d: unknown instruction %s\n", lines, tokens[0]);
 				free(buffer), free_stack(&stack), fclose(fd);
 				free(tokens), exit(EXIT_FAILURE);
 			}
