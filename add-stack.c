ce6c3f7d61cc8aa5025ea7a9a75600e41dae4c18
Stomy1 <110528133+gustonstomy@users.noreply.github.com>
2023-12-15 09:04:25 +0000

    Update add-stack.c

--- add-stack.c
+++ add-stack.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 /**
- * v_add_stack - adds new node to stack
+ * _add_stack - adds new node to stack
  * @head: pointer to pointer in stack
  * @n: element
  * Return: Nothing
  */
-stack_t *v_add_stack(stack_t **head, const int n)
+stack_t *_add_stack(stack_t **head, const int n)
 {
 	stack_t *current, *previ;
 
